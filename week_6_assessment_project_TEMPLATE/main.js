8de0442e45ae9558ef981f497b3b075e0ea5edc4
vinson-han <han_vinson@student.smc.edu>
2023-12-12 18:31:22 -0800

    t

--- week_6_assessment_project_TEMPLATE/main.js
+++ week_6_assessment_project_TEMPLATE/main.js
@@ -6,6 +6,16 @@ console.log("connected to main.js")
     // Test that each works with the fetch request
 const CATEGORY = [
   "Starter",
+  "Beef",
+  "Chicken",
+  "Lamb",
+  "Pasta",
+  "Pork",
+  "Seafood",
+  "Vegetarian",
+  "Vegan",
+  "Side",
+  "Dessert"
 ]
 
 // select element that will store our category options
@@ -21,6 +31,8 @@ for (let i = 0; i < CATEGORY.length; i++) {
 let mealsList = document.getElementById("mealsList");
 
 function getFetch() {
+
+
   const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categories.value}`;
 
   fetch(url)
@@ -28,15 +40,27 @@ function getFetch() {
     .then(data => {
       // the line below clears the HTML each time a new category is requested, so that the items don't stack each time
       mealsList.innerHTML = "";
-
+      console.log(data)
       // per category that is received in the data object,
       // there is a meals array
       // we will use the meals array to access individual meals
       for (let i = 0; i < data.meals.length; i++) {
+        
         let container = document.createElement("div");
         container.classList.add("mealContainer");
         mealsList.appendChild(container);
+        
+        let insideContainer = document.createElement("div")
+        let h2 = document.createElement("h2")
+        h2.textContent = data.meals[i].strMeal
+        let img = document.createElement("img")
+        img.src= data.meals[i].strMealThumb
 
+        insideContainer.appendChild(h2)
+        insideContainer.appendChild(img)
+        container.append(insideContainer)
+
+        console.log(container)
         // use the example above to complete TASK 2
         // TASK 2
         // 1) add a header to the container
